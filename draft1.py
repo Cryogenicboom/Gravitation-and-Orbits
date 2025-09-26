88f995ad86447aed615634cf4356abcf9cd7cf14
Aryan Mishra <aryanxposat@gmail.com>
2025-09-26 19:34:29 +0530

    Multiple planets
    Add multiple planets option

--- draft1.py
+++ draft1.py
@@ -24,43 +24,61 @@ class Planet:
         self.color = color
         self.radius = radius
         self.id = id
+        self.force = 0
         # define starting angle, and this angle will help to make sure of angular speed
         self.theta = 0
     def draw(self):
         self.circle = pygame.draw.circle(screen, self.color, (self.x_pos, self.y_pos), self.radius)
 
 
+    def Force(self):
+        dx = self.x_pos - p1.x_pos
+        dy = self.y_pos - p1.y_pos
+        distance = math.sqrt(dx*dx + dy*dy)
+        self.force = (Gravitation*p1.mass*self.mass)/distance*distance
+        return self
+    
+    def revolve(self):
+        dx = self.x_pos - p1.x_pos
+        dy = self.y_pos - p1.y_pos
+        radius = math.sqrt(dx*dx + dy*dy)
+
+        self.x_pos = p1.x_pos + radius * math.cos(self.theta)
+        self.y_pos = p1.y_pos + radius * math.sin(self.theta)
+        self.theta += angular_speed  
+
 class Orbits:
-    def __init__(self, radius, color, x_pos, y_pos, id,):
-        self.radius = radius
+    def __init__(self, color, x_pos, y_pos, id,):
         self.x_pos = x_pos
         self.y_pos = y_pos
         self.color = color
         self.width = 3
         self.id = id
     def draw(self, x_pos, y_pos):
-        dx = p2.x_pos - p1.x_pos
-        dy = p2.y_pos - p1.y_pos
+        dx = self.x_pos - p1.x_pos
+        dy = self.y_pos - p1.y_pos
         radius = math.sqrt(dx*dx + dy*dy)
         self.circle = pygame.draw.circle(screen, self.color, (p1.x_pos, p1.y_pos), int(radius), self.width)
 
+    
 
 # intilizing instances 
 p1 = Planet(518,332, 0,0, 1000, 'red', 60, 1)
 p2 = Planet(139, 453, 0,0, 400,'blue',30,2)
+p3 = Planet(308,438,0,0, 500, 'green', 20, 3)
 p2.theta = 0
 
-
-dx = p2.x_pos - p1.x_pos
-dy = p2.y_pos - p1.y_pos
-radius = math.sqrt(dx*dx + dy*dy)
-
-o1 = Orbits(radius, 'silver', p1.x_pos,p1.y_pos,9 )
+o1 = Orbits('white', p1.x_pos,p1.y_pos,9 )
+o2 = Orbits('white', p1.x_pos, p1.y_pos,10)
 
 # variables 
 angular_speed = 0.02
-Force = (Gravitation*p1.mass*p2.mass)/radius*2
-print(f"Force = ",Force)
+
+
+force2 = p2.Force()
+force3 = p3.Force()
+print(f"F2 = ", force2, "F3 = ", force3)
+
 
 
 # Game loop 
@@ -70,16 +88,10 @@ while running:
     screen.fill('black')
 
     p1.draw()
-
-    dx = p2.x_pos - p1.x_pos
-    dy = p2.y_pos - p1.y_pos
-    radius = math.sqrt(dx*dx + dy*dy)
-
-    p2.x_pos = p1.x_pos + radius * math.cos(p2.theta)
-    p2.y_pos = p1.y_pos + radius * math.sin(p2.theta)
-    p2.theta += angular_speed  # move along the orbit
-
     p2.draw()
+    p2.revolve()
+    p3.draw()
+    p3.revolve()
     o1.draw(p1.x_pos, p1.y_pos)
 
 
